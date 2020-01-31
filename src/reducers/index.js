511ba3a395a7768d93b050d79a9a1a74c48b761c
kan-kan <kancan877@gmail.com>
2020-01-31 21:47:52 +0900

    refactor events with reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,5 @@
-import { DELETE_ALL_EVENTS, CREATE_EVENT, DELETE_EVENT } from "../actions";
-const events = (state = [], action) => {
-  switch (action.type) {
-    case CREATE_EVENT:
-      let id = state.length === 0 ? 1 : state[state.length - 1].id + 1;
-      return [...state, { id, title: action.title, body: action.body }];
-    case DELETE_EVENT:
-      return state.filter(event => event.id !== action.id);
-    case DELETE_ALL_EVENTS:
-      return [];
-    default:
-      return state;
-  }
-};
+import { combineReducers } from "redux";
 
-export default events;
+import events from "./events";
+
+export default combineReducers({ events });
